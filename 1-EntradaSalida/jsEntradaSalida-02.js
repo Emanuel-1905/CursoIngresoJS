90d4728720ed4886fe85d49ac5e24f19b4a1f217
emanuel-1905 <emanuel.cortes1993@gmail.com>
2021-07-07 22:46:00 -0300

    Actualizacion

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,13 @@
 /*
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
-function mostrar()
-{
-	alert("ok");
-}
+function mostrar() {
+	//declaracion de una variable
+	let nombre
+	
+	
+	nombre = prompt ("Ingrese su nombre: ");
 
+
+	alert(nombre);
+}
--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -2,24 +2,54 @@
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
-ej.: "la Resta es 750"*/ 
-function sumar()
-{	
-	alert("ok");	
+ej.: "la Resta es 750"*/
+function sumar() {
+	let valNum1;
+	let valNum2;
+	let total;
+
+	valNum1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	valNum2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+	total = valNum1 + valNum2;
+
+	alert("El resultado de la suma es " + total);
+
+
 }
 
-function restar()
-{
-	
+function restar() {
+	let valNum1;
+	let valNum2;
+	let total;
+
+	valNum1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	valNum2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+	total = valNum1 - valNum2;
+
+	alert("El resultado de la suma es " + total);
 }
 
-function multiplicar()
-{ 
-	
+function multiplicar() {
+	let valNum1;
+	let valNum2;
+	let total;
+
+	valNum1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	valNum2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+	total = valNum1 * valNum2;
+
+	alert("El resultado de la suma es " + total);
 }
 
-function dividir()
-{
-	
+function dividir() {
+	let valNum1;
+	let valNum2;
+	let total;
+
+	valNum1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	valNum2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+	total = valNum1 / valNum2;
+
+	alert("El resultado de la suma es " + total);
 }
 
