6aac30f54bd2d03e86eed342c07f0c1abea1203c
emanuel-1905 <emanuel.cortes1993@gmail.com>
2021-07-21 21:57:29 -0300

    Nueva actualizacion

--- 5-InstruccionWhile/jsIteracionesWhile-03.js
+++ 5-InstruccionWhile/jsIteracionesWhile-03.js
@@ -2,7 +2,19 @@
 al presionar el botón pedir la CLAVE (ayuda: es utn750)*/
 function mostrar()
 {
-	var claveIngresada;
-	claveIngresada = prompt("ingrese el número clave.");
+	let claveIngresada;
+	 //pido el dato 
+	 clave = prompt ("Ingrese clave:  ");
+
+	 while( clave != "utn750" ){
+		 // tengo que poner una condicion que sea verdadera cuando el dato sea invalido
+		  
+		 // mientras el dato sea invalido, informo que es invalido, lo vuelvo a pedir
 	
+    clave = prompt ("Clave incorrecta. Reingrese clave :  ");
+
+	
+	}
+
+	alert ("Clave correcta!!");
 }//FIN DE LA FUNCIÓN
--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -9,6 +9,70 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
 
  */
 function CalcularPrecio () 
-{
- 	
-}
+{ // 1- Declaro variables
+ 	let precio = 35;
+    let cantidad;
+    let marca;
+    let porcDescuento;
+    let impuesto;
+
+    //2- levantar los datos (cantidad, marca)
+
+    cantidad = parseInt(document.getElementById("txtIdCantidad").value);
+    marca = document.getElementById("Marca").value;
+    
+    switch (cantidad){
+        case 1:
+        case 2:
+            descuento = 0;
+            break;
+        
+        case 3:
+            if (marca == "ArgentinaLuz"){
+                porcDescuento = 0.15;
+            }else if (marca == "FelipeLamparas"){
+                porcDescuento == 0.10;
+            }else{
+                porcDescuento = 0.5;
+            }
+            break;
+
+        case 4:
+            if (marca == "ArgentinaLuz" || marca == "FelipeLamparas"){
+                porcDescuento = 0.25;
+            }else{
+                porcDescuento = 0.20;
+            }
+
+        case 5:
+            if (marca == "ArgentinaLuz"){
+                porcDescuento = 0.40;
+            }else{
+                porcDescuento = 0.30
+            }
+            break;
+    default:
+        porcDescuento = 0.50;
+
+    //Fin del switch
+
+            }
+
+    //3- de acuerdo a la cantidad comprada y a la marca, calculo el porc de descuento
+    
+
+    }
+
+
+    // estrategia de resolucion
+    // 1- Declaro variables
+    //1.1 precio/cantidad/marca/porDescuento/impuestoFinal/IIBB
+    //2- levantar los datos (cantidad, marca)
+    //3- de acuerdo a la cantidad comprada y a la marca, calculo el porc de descuento
+    //4- una vez que tengo el % de descuento, calculo el precio de una lampara con el desc.
+    //5- escribo en la caja de texto inferior el precio unit de la lampara
+    //6- calculo el importe final(precio de la lampara con descuento * la cantidad de lamparas)
+    //7- me fijo si corresponde calcular el impuesto, y de ser asi, lo calculo
+    //8- muestro el importe final y de corresponder el impuesto
+    
+
